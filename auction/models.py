30bfe53a93b75d6826cf44c655dc98845b89c556
Yryskeldi <yryskeldi20051205@gmail.com>
2022-08-09 17:23:38 +0600

    models

--- auction/models.py
+++ auction/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Bet(models.Model):
+    user = models.ForeignKey(User, related_name='bets', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    car = models.ForeignKey(Car, related_name='bets', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.user} -> {self.price}"
+    
+
+
